2de9720edfb1e570bd8f4b615183bef9a92080cb
YuriiKudlai <kudlai.yurii@gmail.com>
2022-06-02 16:17:38 +0300

    update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ingredientsListEl = document.querySelector('#ingredients');
+
+const itemsListItemEl = ingredients.map(ingredient => {
+const createEl = document.createElement("li");
+createEl.classList.add("item");
+  createEl.textContent = `${ingredient}`;
+  console.log(createEl);
+  return createEl;
+  }
+);
+
+
+ingredientsListEl.append(...itemsListItemEl);
+console.log(ingredientsListEl);
