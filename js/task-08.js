d5c6261269ed2e324032101fc43821230c920f39
YuriiKudlai <kudlai.yurii@gmail.com>
2022-06-09 15:23:23 +0300

    update task - 08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
 
+let formEl = document.querySelector('.login-form');
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: {
+            email, password
+        }
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return alert('Будь-ласка заповніть всі поля')
+    }
+    else {
+        const dataForm = {
+            email: email.value,
+            password: password.value
+        }
+        console.log(dataForm);
+    event.currentTarget.reset();
+    }  
+    }
+    
+formEl.addEventListener('submit', onFormSubmit);
