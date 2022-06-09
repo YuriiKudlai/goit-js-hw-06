9bb1c6be9a27d2e0dad751b3a8b5a4a4d2211fbf
YuriiKudlai <kudlai.yurii@gmail.com>
2022-06-09 16:01:32 +0300

    update task - 09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+
+const bodyColor = document.querySelector("body");
+const btnChangeColor = document.querySelector(".change-color")
+const colorName=document.querySelector(".color")
+const changeBtnColor = () => {
+  bodyColor.style.backgroundColor = getRandomHexColor();
+  colorName.textContent = bodyColor.style.backgroundColor;
+}
+btnChangeColor.addEventListener("click", changeBtnColor)
+
+
+
+
+
