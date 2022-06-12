c3b139cac40b158c44ad031b428d1118260e0b9d
YuriiKudlai <kudlai.yurii@gmail.com>
2022-06-12 17:17:01 +0300

    update task - 05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,15 +1,16 @@
 const inputEl = document.querySelector('input#name-input');
-console.log(inputEl);
+
 const outputEl = document.querySelector('span#name-output');
-console.log(outputEl);
+
 
 
 function onInputChange(event) {
-    if (inputEl === '') {
-    outputEl.textContent = 'Anonymous';
+    const userInput = event.currentTarget.value;
+    if (userInput === '') {
+      return outputEl.textContent = 'Anonymous';
     }
-    outputEl.textContent = event.currentTarget.value;
-   
+    outputEl.textContent = userInput;
+
 }
 
 inputEl.addEventListener('input', onInputChange);
